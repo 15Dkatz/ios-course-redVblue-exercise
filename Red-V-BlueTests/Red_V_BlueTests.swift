d6c255bf4a949078c94826d1f7a4a4af0103fca8
David Katz <15Dkatz@shcp.edu>
2015-09-20 20:51:55 -0700

    Initial Commit

---
+++ Red-V-BlueTests/Red_V_BlueTests.swift
@@ -0,0 +1,36 @@
+//
+//  Red_V_BlueTests.swift
+//  Red-V-BlueTests
+//
+//  Created by David Katz on 9/20/15.
+//  Copyright © 2015 davidkatz. All rights reserved.
+//
+
+import XCTest
+@testable import Red_V_Blue
+
+class Red_V_BlueTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
